50afbe80cfc5e8e14dbf4268939736b891f49f94
Abdi MIGAUD <abdi.migaud@laposte.net>
2022-11-08 02:23:51 +0100

    add photographer's constructor

---
+++ js/models/PhotographerModel.js
@@ -0,0 +1,47 @@
+class PhotographerModel {
+    constructor(data) {
+        this._id = data.id;
+        this._photographerId = data.photographerId;
+        this._title = data.title;
+        this._view = data.view;
+        this._likes = data.likes;
+        this._date = data.date;
+        this._price = data.price;
+    }
+    
+    get id() {
+        return this._id;
+    }
+
+    get photographerId() {
+        return this._photographerId;
+    }
+    
+    get title() {
+        return this._title;
+    }
+
+    get view() {
+        return this._view.hasOwnProperty('image') ? this._view.image : this._view.video;
+    }
+
+    get image() {
+        return `/assets/photographers/${this._image}`;
+    }
+
+    get video() {
+        return `/assets/photographers/${this._video}`;
+    }
+
+    get likes() {
+        return this._likes;
+    }
+    
+    get date() {
+        return this._date;
+    }
+
+    get price() {
+        return this._price;
+    }
+}
